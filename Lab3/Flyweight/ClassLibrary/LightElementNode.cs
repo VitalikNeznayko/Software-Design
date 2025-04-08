77f5160715a0d71cdd896ccff35318d840bda574
VitalikNeznayko <vtk241_nvk@student.ztu.edu.ua>
2025-04-08 20:04:58 +0300

    Add lab3

---
+++ Lab3/Flyweight/ClassLibrary/LightElementNode.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace ClassLibrary
+{
+    public class LightElementNode : LightNode
+    {
+        public string TagName { get; }
+        public DisplayType Display { get; }
+        public TagType TagType { get; }
+        public List<string> CssClasses { get; }
+        public List<LightNode> Children { get; }
+
+        public HtmlElementFlyweight Flyweight { get; }
+
+        public LightElementNode(HtmlElementFlyweight flyweight)
+        {
+            Flyweight = flyweight;
+            CssClasses = new List<string>();
+            Children = new List<LightNode>();
+        }
+
+        public void AddChild(LightNode node)
+        {
+            Children.Add(node);
+        }
+
+        public void AddClass(string className)
+        {
+            CssClasses.Add(className);
+        }
+
+        public override string InnerHTML => string.Join("", Children.Select(c => c.OuterHTML));
+
+        public override string OuterHTML
+        {
+            get
+            {
+                var classAttr = CssClasses.Count > 0 ? $" class=\"{string.Join(" ", CssClasses)}\"" : "";
+                if (Flyweight.TagType == TagType.Single)
+                    return $"<{Flyweight.TagName}{classAttr}/>";
+                return $"<{Flyweight.TagName}{classAttr}>{InnerHTML}</{Flyweight.TagName}>\n";
+            }
+        }
+    }
+}
