0be03792f39f7e6f9358175f2715d627051b6f7d
VitalikNeznayko <vtk241_nvk@student.ztu.edu.ua>
2025-04-23 20:33:03 +0300

    Observer

--- Lab3/Composite/ClassLibrary/LightElementNode.cs
+++ Lab3/Composite/ClassLibrary/LightElementNode.cs
@@ -20,6 +20,7 @@ namespace ClassLibrary
         public TagType TagType { get; }
         public List<string> CssClasses { get; }
         public List<LightNode> Children { get; }
+        private Dictionary<string, List<Action>> eventListeners;
 
         public LightElementNode(string tagName, DisplayType display, TagType tagType)
         {
@@ -28,6 +29,7 @@ namespace ClassLibrary
             TagType = tagType;
             CssClasses = new List<string>();
             Children = new List<LightNode>();
+            eventListeners = new Dictionary<string, List<Action>>();
         }
 
         public void AddChild(LightNode node)
@@ -38,6 +40,31 @@ namespace ClassLibrary
         public void AddClass(string className)
         {
             CssClasses.Add(className);
+        }
+
+        public void AddEventListener(string eventType, Action handler)
+        {
+            if (!eventListeners.ContainsKey(eventType))
+            {
+                eventListeners[eventType] = new List<Action>();
+            }
+            eventListeners[eventType].Add(handler);
+        }
+
+        public void TriggerEvent(string eventType)
+        {
+            if (eventListeners.ContainsKey(eventType))
+            {
+                Console.WriteLine($"Triggering '{eventType}' event on <{TagName}>");
+                foreach (var handler in eventListeners[eventType])
+                {
+                    handler();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No listeners for '{eventType}' event on <{TagName}>");
+            }
         }
 
         public override string InnerHTML => string.Join("", Children.Select(c => c.OuterHTML));
--- Lab3/Composite/Composite/Program.cs
+++ Lab3/Composite/Composite/Program.cs
@@ -3,20 +3,44 @@ public class Program
 {
     public static void Main()
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         var div = new LightElementNode("div", DisplayType.Block, TagType.Pair);
         div.AddClass("container");
 
-        var h1 = new LightElementNode("h1", DisplayType.Block, TagType.Pair);
-        h1.AddClass("tittle");
-        h1.AddChild(new LightTextNode("Назва"));
-
         var p = new LightElementNode("p", DisplayType.Block, TagType.Pair);
-        p.AddChild(new LightTextNode("Текст"));
-
-        div.AddChild(h1);
+        p.AddChild(new LightTextNode("Hello, World!"));
         div.AddChild(p);
 
-        Console.WriteLine();
+        var button = new LightElementNode("button", DisplayType.Inline, TagType.Pair);
+        button.AddChild(new LightTextNode("Click me"));
+        div.AddChild(button);
+
+        button.AddEventListener("click", () =>
+            Console.WriteLine("<button> was clicked!")
+        );
+        button.AddEventListener("click", () =>
+            Console.WriteLine("Another click handler triggered!")
+        );
+        button.AddEventListener("mouseover", () =>
+            Console.WriteLine("Mouse hovered over the button!")
+        );
+
+        p.AddEventListener("click", () =>
+            Console.WriteLine("<p> was clicked!")
+        );
+
         Console.WriteLine(div.OuterHTML);
+        Console.WriteLine();
+
+        button.TriggerEvent("click");
+        Console.WriteLine();
+
+        button.TriggerEvent("mouseover");
+        Console.WriteLine();
+
+        p.TriggerEvent("click");
+        Console.WriteLine();
+        p.TriggerEvent("mouseover");
     }
 }
